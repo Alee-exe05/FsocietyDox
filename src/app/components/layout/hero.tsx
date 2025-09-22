20e96c77d39218e8279d979713898f3c5ae569d6
Alessandro Quaranta <alessandroquaranta182@gmail.com>
2025-09-22 17:35:41 +0000

    CI SONO SOLO PAESAGGI

--- src/app/components/layout/hero.tsx
+++ src/app/components/layout/hero.tsx
@@ -1,16 +1,12 @@
-import Image from 'next/image';
+import { FsocietyLogo } from '@/app/components/FsocietyLogo';
 
 export function Hero() {
   return (
-    <div className="relative h-[400px] w-full flex items-center justify-center text-white overflow-hidden">
-      <Image
-        src="https://picsum.photos/seed/hacker-mask/1200/400"
-        alt="Hacker mask"
-        fill
-        className="object-cover object-center"
-        data-ai-hint="hacker mask"
-      />
+    <div className="relative h-[400px] w-full flex items-center justify-center text-white overflow-hidden bg-black">
       <div className="absolute inset-0 bg-black/50 z-10" />
+      <div className="z-20 text-primary scale-[3]">
+        <FsocietyLogo />
+      </div>
     </div>
   );
 }
