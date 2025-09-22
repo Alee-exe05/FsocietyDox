c0962b55f351030a12c937d52d44b8cfc0f8622f
Alessandro Quaranta <alessandroquaranta182@gmail.com>
2025-09-22 17:49:04 +0000

    non carica esce

--- src/app/components/layout/hero.tsx
+++ src/app/components/layout/hero.tsx
@@ -4,7 +4,7 @@ export function Hero() {
   return (
     <div className="relative h-[400px] w-full flex items-center justify-center text-white overflow-hidden">
       <Image
-        src="https://images.unsplash.com/photo-1519219322305-6804b46b6279?q=80&w=2070"
+        src="https://images.unsplash.com/photo-1618362361133-d1d3bdd41418?q=80&w=2070"
         alt="fsociety mask"
         fill
         className="object-cover"
