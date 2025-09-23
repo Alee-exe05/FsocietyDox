1fb5fc0bc3c83659d06ce01cbbecd58679358833
Alessandro Quaranta <alessandroquaranta182@gmail.com>
2025-09-23 15:30:39 +0000

    cancellal (_for element div_)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,6 @@
 import React from 'react';
 import { AppHeader } from '@/app/components/layout/app-header';
 import { ModulesTable } from '@/app/components/dashboard/modules-table';
-import { ModerationTool } from '@/app/components/dashboard/moderation-tool';
 import { Hero } from '@/app/components/layout/hero';
 import { useLanguage } from '@/contexts/language-context';
 
@@ -15,14 +14,6 @@ export default function Home() {
       <main className="flex-1">
         <Hero />
         <div className="p-4 md:p-10 space-y-8">
-            <div className="grid gap-4 md:gap-8 lg:grid-cols-2 xl:grid-cols-3">
-                <div className="xl:col-span-2">
-                    <ModerationTool />
-                </div>
-                <div>
-                    
-                </div>
-            </div>
             <ModulesTable />
         </div>
       </main>
